a229d6eb96c9ea7e913d432497d4e96a46c56eb1
Jayesh Patel <ejayesh@hotmail.com>
2017-01-27 12:59:50 +0000

    fix escape of variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,10 +44,10 @@ http {
 
   location / {
     proxy_pass http://${PROXY_TARGET}:${TARGET_PORT};
-    proxy_set_header Host $http_host;
-    proxy_set_header X-Real-IP $remote_addr;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header Host \$http_host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
   }
   
   }
